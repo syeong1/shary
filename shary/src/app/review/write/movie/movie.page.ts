b0ca53d87e1f191cc0bab6bb74cd6f747d5ca4bd
sgjh4040 <sgjh4040@naver.com>
2019-08-13 19:37:48 +0900

    <새 write movie 페이지> -페이지 위치 이동

--- shary/src/app/review/write/movie/movie.page.ts
+++ shary/src/app/review/write/movie/movie.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MovieService } from 'src/app/services/movie.service';
+import { ModalController } from '@ionic/angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-movie',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.page.scss'],
 })
 export class MoviePage implements OnInit {
+  reviewForm: FormGroup;
+  movie: null;
 
-  constructor() { }
+  constructor(private activateRoute: ActivatedRoute,private movieService: MovieService, private modalController:ModalController) { }
 
   ngOnInit() {
+    this.reviewForm = new FormGroup({
+      title: new FormControl('',[Validators.required]),
+      writer: new FormControl(''),
+      reviewlist_id: new FormControl(''),
+      release_date: new FormControl(''),
+      overview: new FormControl(''),
+      genre: new FormControl(''),
+      watch_date: new FormControl(''),
+      poster_path: new FormControl(''),
+      rating: new FormControl(''),
+      famouse_line: new FormControl(''),
+      review: new FormControl(''),
+      tags: new FormControl(''),
+
+    })
+    
   }
+  async openSearchMovieModal(){
+    const modal = await this.modalController.create({
+      component: MoviePage,
+    });
+    modal.onDidDismiss()
+    .then((data) => {
+      
+      console.log(data['data']);
+      console.log(typeof data['data']);
+
+      this.movie = data['data'];
+    })
+    return await modal.present(); 
+  };
 
 }
