aff752e582176fd01b1520a6173fe5f69527d99a
Cheolhyeon Park <fepanbr@gmail.com>
2019-08-18 22:32:32 +0900

    - reviewbook create category 확인

--- shary/src/app/reviewbook/list/list.page.ts
+++ shary/src/app/reviewbook/list/list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
+import { FoodService } from 'src/app/services/food.service';
 
 @Component({
   selector: 'app-list',
@@ -11,11 +12,14 @@ export class ListPage implements OnInit {
   
   category: any;
 
-  constructor(private route: ActivatedRoute, private router: Router) { 
+  constructor(private route: ActivatedRoute, private router: Router, private foodService: FoodService) { 
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.category = this.router.getCurrentNavigation().extras.state.category;
         console.log('넘어온 카테고리 : ' + this.category);
+        this.foodService.getReviewBookList(this.category).subscribe((results) =>{
+          console.log(results);
+        })
       }
     })
    }
@@ -30,6 +34,7 @@ export class ListPage implements OnInit {
    }
 
   ngOnInit() {
+    
   }
 
 }
