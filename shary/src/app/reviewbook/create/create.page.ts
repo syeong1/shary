d509e220f8c9b865d69d21e90eaa16fbfca967f1
Cheolhyeon Park <fepanbr@gmail.com>
2019-08-18 22:31:50 +0900

    - reviewbook create food 추가

--- shary/src/app/reviewbook/create/create.page.ts
+++ shary/src/app/reviewbook/create/create.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FoodService } from 'src/app/services/food.service';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatePage implements OnInit {
 
-  constructor() { }
+  category: any;
+  reviewForm: FormGroup;
+
+  constructor(private route: ActivatedRoute, private router: Router, private foodService: FoodService) {
+    this.route.queryParams.subscribe(params => {
+      // console.log(this.router.getCurrentNavigation().extras.state);
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.category = this.router.getCurrentNavigation().extras.state.category;
+        console.log('넘어온 카테고리 : ' + this.category);
+      }
+    })
+  }
 
   ngOnInit() {
+    this.reviewForm = new FormGroup({
+      title: new FormControl('',[Validators.required]),
+      type: new FormControl('', [Validators.required])
+    })
+  }
+
+  onSubmit() {
+    this.foodService.writeReviewBook(this.reviewForm.value).subscribe((res) => {
+      console.log(this.reviewForm);
+      this.router.navigate(['reviewbook/list'], this.category);
+    })
   }
 
 }
