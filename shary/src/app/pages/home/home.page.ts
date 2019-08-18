f38bb911ee14c1c535d44421e9f18c4650ba20c0
Cheolhyeon Park <fepanbr@gmail.com>
2019-08-18 16:56:16 +0900

    - 'home'페이지에서 type을 가지고 'list'페이지로 이동

--- shary/src/app/pages/home/home.page.ts
+++ shary/src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  category: string = '';
+
+  constructor(private router: Router) { }
+
+  
+  /**
+   * type에 맞는 reviewBook List 페이지로 이동
+   * @param {string} type
+   * use 'Router.navigate' with navigationExtras
+   */
+  openMyListByType(type) {
+    let navigationExtras: NavigationExtras = type;
+    this.router.navigate(['/list'], navigationExtras);
+  }
+
+  openBookList() {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        category: '책'
+      }
+    };
+    this.router.navigate(['/reviewbook/list'], navigationExtras);
+  }
+  openMovieList() {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        category: '영화'
+      }
+    };
+    this.router.navigate(['/reviewbook/list'], navigationExtras);
+  }
+  /**
+   * food 타입을 list페이지에 전달
+   */
+  openFoodList() {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        category: 'food'
+      }
+    };
+    this.router.navigate(['/reviewbook/list'], navigationExtras);
+  }
+  openTVList() {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        category: 'TV'
+      }
+    };
+    this.router.navigate(['/reviewbook/list'], navigationExtras);
+  }
 
   ngOnInit() {
   }
