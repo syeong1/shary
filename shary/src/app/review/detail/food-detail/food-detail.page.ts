f42796e43f00b4500aa09601c3d478c2588d3d6f
CheolHyeon <fepanbr@gmail.com>
2019-08-22 23:32:56 +0900

    - reveiwId 받아오는 부분 snapshot으로 변경 - food-list page 충돌 부분 견경

--- shary/src/app/review/detail/food-detail/food-detail.page.ts
+++ shary/src/app/review/detail/food-detail/food-detail.page.ts
@@ -9,20 +9,16 @@ import { FoodService } from 'src/app/services/food.service';
 })
 export class FoodDetailPage implements OnInit {
 
+  reviewId;
+
   details;
 
-  constructor(private activatedRoute: ActivatedRoute, private foodService: FoodService) { 
-    this.activatedRoute.params.subscribe(params => {
-      this.foodService.getReviewDetail(params['id']).subscribe(data => {
-        console.log('food list에 받아온 data', data);
-        this.details = data;
-      })
-      console.log(`가져온 리뷰 id : ${params['id']}`);
-      console.log(`details : ${this.details}`);
-      });
-  }
+  constructor(private activatedRoute: ActivatedRoute, private foodService: FoodService) {  }
 
   ngOnInit() {
+    this.reviewId = this.activatedRoute.snapshot.params['id'];
   }
+
+  
 
 }
--- shary/src/app/review/list/food-list/food-list.page.ts
+++ shary/src/app/review/list/food-list/food-list.page.ts
@@ -10,30 +10,31 @@ import { Observable } from 'rxjs';
 })
 export class FoodListPage implements OnInit {
 
-  reviewId;
+  reviewbookId;
   reviews;
 
-  constructor(private foodService: FoodService, private activatedRoute: ActivatedRoute, private router: Router) { 
-    this.activatedRoute.params.subscribe(params => {
-      this.reviewId = params['id'];
-      this.foodService.getFoodReviewList(this.reviewId).subscribe(data => {
-        console.log('food list에 받아온 data', data);
-        this.reviews = data;
-      })
-      console.log(`가져온 리뷰 리스트 id : ${this.reviewId}`);
-      console.log(`reviews : ${this.reviews}`);
-      });
-  }
+  constructor(private foodService: FoodService, private activatedRoute: ActivatedRoute, private router: Router) {  }
 
   ngOnInit() {
+    this.reviewbookId = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log('review 리뷰북 id : ', this.reviewbookId);
+    this.getReviews();
   }
 
   openWritePage(){
-    this.router.navigate(['/food/write', this.reviewId]);
+    this.router.navigate(['/food/write', this.reviewbookId]);
   }
 
   openDetailPage(review) {
     this.router.navigate(['/food/detail', review._id]);
+  }
+
+  getReviews() {
+    this.foodService.getFoodReviewList(this.reviewbookId).subscribe(data => {
+      console.log('리뷰 리스트 Service 요청할 때 id : ', this.reviewbookId);
+      console.log('받아온 Reviews data', data);
+      this.reviews = data;
+    })
   }
 
 }
