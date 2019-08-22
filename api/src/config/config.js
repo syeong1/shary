58ba2b2ea3c7d32ae3e4f168a59109d69326ef8a
Cheolhyeon Park <fepanbr@gmail.com>
2019-08-22 15:26:42 +0900

    - config

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -4,7 +4,10 @@ module.exports = {
 
     //네이버 open api 
     client_id = '4BAZEL9fhMV7MnBiJx1T',
-    client_secret = 'BWYeR798st'
+    client_secret = 'BWYeR798st',
 
+    //네이버 지도 api
+    NAVER_CLIENT_ID     = 'yce4qlo07o',
+    NAVER_CLIENT_SECRET = 'NhCdEtu13Zfr2MB9UreEnhRhPEWjEvFKMk4s5ull'
 
 };
