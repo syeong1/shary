5cc123cff2cb7eb16b49588c95c31e5eb7ff3159
Cheolhyeon Park <fepanbr@gmail.com>
2019-08-22 15:02:59 +0900

    - map api key

--- api/src/controller/map-controller.js
+++ api/src/controller/map-controller.js
@@ -1,5 +1,5 @@
-const NAVER_CLIENT_ID     = 'yce4qlo07o'
-const NAVER_CLIENT_SECRET = 'NhCdEtu13Zfr2MB9UreEnhRhPEWjEvFKMk4s5ull'
+
+const config = require('../config/config')
 const request = require('request');
 
 var map = {
@@ -14,8 +14,8 @@ var map = {
             uri:'https://naveropenapi.apigw.ntruss.com/map-place/v1/search', //xml 요청 주소는 https://openapi.naver.com/v1/search/image.xml
             qs :option,
             headers:{
-              'X-NCP-APIGW-API-KEY-ID':NAVER_CLIENT_ID,
-              'X-NCP-APIGW-API-KEY':NAVER_CLIENT_SECRET
+              'X-NCP-APIGW-API-KEY-ID':config.NAVER_CLIENT_ID,
+              'X-NCP-APIGW-API-KEY':config.NAVER_CLIENT_SECRET
             }
           }, function(err, response, body) {
             console.log(body);
