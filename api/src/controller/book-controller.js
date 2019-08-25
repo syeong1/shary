9f135c3562bb270f09c00378265e98e2e6059d1b
syeong <tjsdud7379@gmail.com>
2019-08-25 20:02:56 +0900

    <routes> 리뷰북, 책리뷰 전체적으로 수정
    <book-controller>
    editReview, deleteReview, getReviewDetail 추가
    
    <reviewbook-controller>
    - getReviewList() 리뷰북 길이에 따라 상태 반환
    - deleteReviewbook 추가
    
    <book>
    - reviewList -> reviewbook 변경
    - publisher:Date 삭제

--- api/src/controller/book-controller.js
+++ api/src/controller/book-controller.js
@@ -27,17 +27,19 @@ exports.getBookData = (req, res) => {
 
 // 책 리뷰 작성
 exports.writeReview = (req, res) => {
+
     let newReview = Book(req.body);
     let writer = req.user._id;
     newReview.writer = writer;
 
-    newReview.save((err, result) => {
+    newReview.save((err, book) => {
         if (err) {
+            console.log(err);
             return res.status(400).json({
                 'msg': err
             });
         }
-        console.log(result);
+        console.log(book);
         return res.status(201).json({
             'msg': '등록되었습니다'
         });
@@ -45,28 +47,93 @@ exports.writeReview = (req, res) => {
 }
 
 
-// 책 리뷰리스트 가져오기
-exports.getBookReviewList = (req, res) => {
-    
-    console.log(req);
-        console.log('요청한 리뷰리스트 id', req.params._id);
-        // let reviewbook_id = req.params.id;
-        // 책 리뷰북 가져오기
-        let reviewbook_id = req.params.id;
-            Book.find({reviewList: reviewbook_id}, function (err, reviews) {
-            console.log(reviews);
-            if (err) {
-                return res.status(500).json({
-                    error: err
-                });
-            }
-            if (!reviews) {
-                return res.status(404).json({
-                    error: 'reviewbook not found'
-                });
-            }
-            if (reviews) {
-                return res.status(200).json(reviews);
-            }
+exports.editReview = (req, res) => {
+
+    console.log("@@@ editReview @@@");
+    console.log('수정할 review_id : ', req.params.id);
+    console.log('수정할 정보: ', req.body);
+    console.log("=================================================")
+    Book.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, function (err, book) {
+        if (err) {
+            console.log(err);
+        }
+        return res.status(201).json({
+            'msg': '리뷰 업데이트 성공',
+            'result': book
         });
+    });
+}
+
+
+// 책 리뷰 삭제
+exports.deleteReview = (req, res) => {
+    let review_id = req.params.id;
+    Book.findByIdAndDelete(review_id, (err, book) => {
+        if (err) {
+            return res.status(400).json({
+                'msg': err
+            });
+        };
+        console.log('삭제완료 book:', book);
+        return res.json(book);
+    })
+}
+
+// 책 리뷰 리스트 가져오기
+exports.getBookReviewList = (req, res) => {
+
+    console.log('### 요청한 리뷰리스트 id : ', req.params.id);
+
+    let reviewbook_id = req.params.id;
+
+    Book.find({
+        reviewbook: reviewbook_id, // 리뷰북 id로 검색
+        writer: req.user._id
+    }, function (err, books) {
+        console.log(books);
+        if (err) {
+            return res.status(500).json({
+                error: err
+            });
+        }
+        if (!books) {
+            return res.status(404).json({
+                error: 'reviewbook not found'
+            });
+        }
+        if (books) {
+            return res.status(200).json(books);
+        }
+    });
+}
+
+
+
+// 책 리뷰 디테일 가져오기
+exports.getBookReviewDetail = (req, res) => {
+
+    console.log('#### 요청한 리뷰 id : ', req.params.id);
+    let review_id = req.params.id;
+
+    Book.findOne({
+        _id: review_id, // 리뷰 id로 검색
+        writer: req.user._id
+    }, function (err, book) {
+        console.log(book);
+        if (err) {
+            return res.status(500).json({
+                error: err
+            });
+        }
+        if (!book) {
+            return res.status(404).json({
+                error: 'book detail not found'
+            });
+        }
+        if (book) {
+            return res.status(200).json(book);
+        }
+    });
 }
