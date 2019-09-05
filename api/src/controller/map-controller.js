94cc3a2e65b3ad7515257f155da6cbd9f705b55f
CheolHyeon <fepanbr@gmail.com>
2019-09-06 00:19:21 +0900

    map-controller 주석 제거

--- api/src/controller/map-controller.js
+++ api/src/controller/map-controller.js
@@ -24,25 +24,3 @@ var map = {
 }
 
 module.exports = map;
-
-
-// routes.get('/map',(req, res, next) => {
-//     const option = {
-//         query  : req.query.name,
-//         coordinate: req.query.coordinate
-//     }
-//     console.log('request query = ' + req.query.query);
-//     console.log('request coordinate = ' + req.query.coordinate);
-//     request.get({
-//         uri:'https://naveropenapi.apigw.ntruss.com/map-place/v1/search', //xml 요청 주소는 https://openapi.naver.com/v1/search/image.xml
-//         qs :option,
-//         headers:{
-//           'X-NCP-APIGW-API-KEY-ID':NAVER_CLIENT_ID,
-//           'X-NCP-APIGW-API-KEY':NAVER_CLIENT_SECRET
-//         }
-//       }, function(err, response, body) {
-//         console.log(body);
-//         res.send(body);
-//       });
-
-// })
