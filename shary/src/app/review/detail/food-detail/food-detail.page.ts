ea3ff970b54d4f2d84bd6b9d0656c8e552a5571b
CheolHyeon <fepanbr@gmail.com>
2019-09-05 14:34:16 +0900

    food detail 컴포넌트 및 지도 추가

--- shary/src/app/review/detail/food-detail/food-detail.page.ts
+++ shary/src/app/review/detail/food-detail/food-detail.page.ts
@@ -1,29 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FoodService } from 'src/app/services/food.service';
 import { ReviewService } from 'src/app/services/review.service';
-import { AlertController } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
 
+
+declare const naver;
+declare const google;
 @Component({
   selector: 'app-food-detail',
   templateUrl: './food-detail.page.html',
   styleUrls: ['./food-detail.page.scss'],
 })
-export class FoodDetailPage implements OnInit {
+export class FoodDetailPage implements OnInit, AfterViewInit {
 
-  reviewId;
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  marker;
 
-  data;
+  reviewId: string;
+  data = null;
 
   constructor(private activatedRoute: ActivatedRoute, private reviewService: ReviewService,
     private router: Router, private alertController: AlertController) {  }
 
   ngOnInit() {
-    this.reviewId = this.activatedRoute.snapshot.params['id'];
+    this.reviewId = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log('맛집 리뷰 id : ', this.reviewId);
+    
   }
 
-  ionViewWillEnter() {
+   ionViewWillEnter() {
     this.getReviewDetail();
+  }
+
+  ngAfterViewInit() {
+     
   }
 
   getReviewDetail() {
@@ -31,6 +42,10 @@ export class FoodDetailPage implements OnInit {
       console.log('*** reviewService.getReviewDetail 요청 때 reviewid : ', this.reviewId);
       console.log('받아온 Review data', data);
       this.data = data;
+      let stringData = JSON.stringify(data);
+      let jsonData = JSON.parse(stringData);
+      console.log('jsonData', jsonData);
+      this.MapInit(jsonData.country, jsonData.x, jsonData.y); 
     })
   }
 
@@ -50,10 +65,10 @@ export class FoodDetailPage implements OnInit {
           text: '확인',
           handler: () => {
             console.log('삭제 확인');
-            this.reviewService.deleteReview('book', this.reviewId).subscribe(data => {
+            this.reviewService.deleteReview('food', this.reviewId).subscribe(data => {
               // 이동할 리뷰북 리스트 아이디
               let reviewbookId = data['reviewbook']
-              this.router.navigate(['book/list', reviewbookId]);
+              this.router.navigate(['food/list', reviewbookId]);
             })
           }
         }
@@ -67,4 +82,64 @@ export class FoodDetailPage implements OnInit {
     this.router.navigate(['food/edit', this.reviewId]);
   }
 
+//   markMap(country, x, y){
+//     if(country === 'oversea'){
+//       let latLng = new google.maps.LatLng(y, x);
+//       this.marker = new google.maps.Marker({
+//           map: this.map,
+//           position: latLng
+//         })
+//         this.map.setCenter(latLng);
+//     }else{
+      
+//       this.marker = new naver.maps.Marker({
+//           map: this.map,
+//           position: latLng
+//         })
+//         this.map.setCenter(latLng);
+//   }
+// }
+
+  
+  MapInit(country, x, y) {
+      if(country === 'oversea'){
+        let latLng = new google.maps.LatLng(y, x);
+        let mapOptions = {
+          latLng: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+        this.marker = new google.maps.Marker({
+          map: this.map,
+          position: latLng
+        })
+        this.map.setCenter(latLng);
+      }else{
+        let latLng = new naver.maps.LatLng(y, x);
+        let mapOptions = {
+          latLng: latLng,
+          zoom: 10
+        }
+        this.map = new naver.maps.Map(this.mapElement.nativeElement, mapOptions);
+        this.marker = new naver.maps.Marker({
+          map: this.map,
+          position: latLng
+        })
+        this.map.setCenter(latLng);
+      }
+
+      // let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      // let current = new naver.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      // let mapOptions = {
+      //   center: latLng,
+      //   zoom: 15,
+      //   mapTypeId: google.maps.MapTypeId.ROADMAP
+      // }
+
+      // this.getAddressFromCoords(resp.coords.latitude, resp.coords.longitude);
+
+      // this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    }
 }
