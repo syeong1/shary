6781138c6deb23f2eb82d1e5763d3681c43cf79b
syeong <tjsdud7379@gmail.com>
2020-03-03 17:22:05 +0900

    좋아요 누른 사람 닉네임 출력

--- src/app/review/detail/liker/liker.page.ts
+++ src/app/review/detail/liker/liker.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ReviewService } from 'src/app/services/review.service';
+import { UserService } from 'src/app/services/user.service';
+
 
 @Component({
   selector: 'app-liker',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liker.page.scss'],
 })
 export class LikerPage implements OnInit {
+ 
+  @Input("reivewId") reivewId;
 
-  constructor() { }
+  reviewId: string;
+  data = null;
+
+
+  constructor(private userService: UserService, private reviewService: ReviewService, private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
+  console.log('음악 리뷰북 id : ', this.reviewId);
+    this.getReviewDetail();
   }
 
+  ionViewWillEnter() {
+    this.getReviewDetail();
+  }
+
+  getReviewDetail() {
+    this.reviewService.getReviewDetail('music', this.reviewId).subscribe(data => {
+      console.log('*** reviewService.getReviewDetail 요청 때 reviewid : ', this.reviewId);
+      console.log('받아온 Review data', data);
+      console.log("data[liker]", data['liker']);
+      this.data = data;
+    })
+  }
 }
